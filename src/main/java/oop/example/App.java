fd9fee767b5d1e798502bb65191c0612ac36babd
MicahPB01 <micahpuccioball@gmail.com>
2021-06-05 21:03:44 -0400

    added copyright

--- src/main/java/oop/example/App.java
+++ src/main/java/oop/example/App.java
@@ -1,13 +1,15 @@
 package oop.example;
 
 import java.util.Scanner;
-
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Micah Puccio-Ball
+ */
 public class App 
 {
     public static void main( String[] args )
     {
         Scanner input = new Scanner(System.in);
-        Scanner stateInput = new Scanner(System.in);
         double orderAmount;
         double stateTax;
         double subTotal;
@@ -17,7 +19,7 @@ public class App
         orderAmount = Double.parseDouble(input.nextLine());
 
         System.out.printf("What is the state? ");
-        String state = stateInput.next();
+        String state = input.next();
 
         subTotal = orderAmount;
         total = subTotal;
@@ -25,6 +27,7 @@ public class App
 
         if(state.equals("WI"))
         {
+
             total  = subTotal * 1.055;
             total = Math.round(total * 100.0) / 100.0;
             stateTax = total - subTotal;
@@ -34,7 +37,6 @@ public class App
         }
 
         System.out.printf("The total is $%.2f", total);
-
 
     }
 }
